957d4c72773d0ffab62c687efaf78bf5ef9cbe13
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-10 16:25:47 +0600

    task deleted using delete modal

---
+++ src/components/DeleteModal.js
@@ -0,0 +1,50 @@
+import React from "react";
+import toast, { Toaster } from "react-hot-toast";
+import { useDeleteTaskMutation } from "../redux/slices/apiSlice";
+
+export default function DeleteModal({ view, setModal, id }) {
+  const [deleteTask] = useDeleteTaskMutation();
+  if (!view) {
+    return null;
+  }
+  const closeModal = (e) => {
+    if (e.target.id === "container") {
+      setModal((m) => !m);
+    }
+  };
+  const handleDelete = async () => {
+    const res = await deleteTask(id);
+    if (res.data.deletedCount === 1) {
+      toast.success("Task Deleted Successfully");
+      setModal((m) => !m);
+    }
+  };
+  return (
+    <>
+      <Toaster />
+      <div
+        id="container"
+        className="bg-opacity-50 bg-white shad backdrop-blur-lg fixed inset-0 z-20 py-10 flex justify-center items-center"
+        onClick={closeModal}
+      >
+        <div className="shad p-10 rounded-lg">
+          <h1 className="my-10">Are you sure you want to delete this task</h1>
+          <div>
+            <button
+              className="text-white font-bold px-10 py-2 bg-red-500"
+              onClick={handleDelete}
+            >
+              Delete
+            </button>
+            <button
+              className="text-white font-bold px-10 mx-10 py-2 bg-gray-500"
+              onClick={() => setModal((m) => !m)}
+            >
+              Cancel
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import DeleteModal from "./DeleteModal";
 
 export default function Task({ task }) {
+  const [deleteModal, setDeleteModal] = useState(false);
   return (
-    <div>
+    <div className="p-5 border-2 border-gray-200 rounded-lg my-2">
       <h1>{task.title}</h1>
       <h1>{task.description}</h1>
+      <button className="bg-red-500 px-5 py-2 mt-3" onClick={() => setDeleteModal(true)}>Delete</button>
+      <div>
+        {deleteModal && (
+          <DeleteModal
+            view={deleteModal}
+            setModal={setDeleteModal}
+            id={task.id}
+          />
+        )}
+      </div>
     </div>
   );
 }
